5492a1633ae66dcd8b594bacdf95bdc07b55b528
solorzadev <osolorzano22@gmail.com>
2022-10-21 07:34:44 -0500

    finalicé el curso

--- server/02-map.js
+++ server/02-map.js
@@ -9,3 +9,16 @@ const newArray = letters.map(item => item + "++");
 
 console.log("Original", letters);
 console.log("New", newArray);
+
+
+const array = [{age: 1}, {age:2}]; 
+const rta = array.map(item => {   
+  item.name = 'My name';
+  return item; 
+})
+console.log("array", array);
+console.log("rta", rta);
+
+const array2 = ['a', 'bb', 'ccc']; 
+const rta2 = array2.map(item => item.length);
+console.log("rta2", rta2);
